17d2ff29a93902616e3dbdcef2b7ccd76b85d3d6
中島萌梨 <nakajimamoeri@Mac.lan>
2026-03-13 16:49:54 +0900

    add login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 export default function LoginPage() {
     return (
         <main>
             <h1>ログイン</h1>
-            <p>ここにログインフォームを作ります。</p>
+
+            <div>
+                <input placeholder="メールアドレス" />
+            </div>
+
+            <div>
+                <input type="password" placeholder="パスワード" />
+            </div>
+
+            <button>ログイン</button>
         </main>
     );
 }
